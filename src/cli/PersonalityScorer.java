af6d1010abcc4891d43e5c87f6d9ef8490004763
mithulya-s <mkumarapeli4@gmail.com>
2025-11-02 23:36:54 +0530

    Modularized the personality determining classes.

---
+++ src/cli/PersonalityScorer.java
@@ -0,0 +1,37 @@
+package cli;
+
+import utilities.PersonalityClassifier;
+import utilities.PersonalityType;
+
+import java.util.Scanner;
+
+public class PersonalityScorer {
+    public int promptForPersonality() {
+        Scanner sc = new Scanner(System.in);
+        int score =0;
+
+        System.out.println("Please answer the following questions: ");
+
+        //looping and calcualting the total
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Question " + i + ": ");
+            int answer = sc.nextInt();
+            while (answer <1 || answer > 5) {
+                System.out.println("Invalid answer. Please enter a number between 1 and 5");
+                answer = sc.nextInt();
+            }
+            //if the answer is correct, addign to the total
+            score += answer;
+        }
+        //normalizing the score for the final score
+        int personalityScore = score*4;
+
+
+        //Assigning the personality score
+        PersonalityType personality = PersonalityClassifier.classifyPersonalityType(personalityScore);
+
+        //no need to return the persoality type since the class returns from the top,so we're returning the score only.
+        return personalityScore;
+
+    }
+}
---
+++ src/utilities/PersonalityClassifier.java
@@ -0,0 +1,16 @@
+package utilities;
+
+public class PersonalityClassifier {
+
+    //think about moving this to util and make it static
+    public static PersonalityType classifyPersonalityType(int score) {
+        if (score >= 90 && score <= 100) {
+            return PersonalityType.LEADER;
+        } else if (score <= 89 && score >= 70) {
+            return  PersonalityType.BALANCED;
+        } else if (score <= 69 && score >= 50) {
+            return  PersonalityType.THINKER;
+        } else return null; //add the other ones
+    }
+}
+
