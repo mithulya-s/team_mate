36ac8a7d515d64d1fe0c78a0c23d79a4e6241261
mithulya-s <mkumarapeli4@gmail.com>
2025-11-05 20:37:30 +0530

    Basic strategy

--- src/services/TeamBuilder.java
+++ src/services/TeamBuilder.java
@@ -1,5 +1,22 @@
 package services;
 
+import base.Participant;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TeamBuilder {
-    //Contains breaking strategy
+    //Contains teaming strategy
+
+    public static List<List<Participant>> dummyFormation(List<Participant> participants, int teamSize) {
+        List<List<Participant>> formedTeams = new ArrayList<>();
+
+        //just putting them into chunks
+        for (int i = 0; i < participants.size(); i += teamSize) {
+            int end = Math.min(i + teamSize, participants.size());
+            formedTeams.add(new ArrayList<>(participants.subList(i, end)));
+        }
+
+        return formedTeams;
+    }
 }
