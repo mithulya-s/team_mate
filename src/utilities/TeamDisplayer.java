25a90a220c1c2d63e182162c8976933f1d79cb16
mithulya-s <mkumarapeli4@gmail.com>
2025-11-07 20:29:42 +0530

    Class to display formed teams

---
+++ src/utilities/TeamDisplayer.java
@@ -0,0 +1,32 @@
+package utilities;
+
+import base.Participant;
+
+import java.util.List;
+
+public class TeamDisplayer {
+
+    //function to display formed teams
+    public static void displayTeams(List<List<Participant>> formedTeams){
+        System.out.println("\nFormed Teams:");
+
+        for (int i = 0; i<formedTeams.size(); i++){
+            List<Participant> team = formedTeams.get(i);
+            System.out.print("Team " + (i+1) + ": ");
+
+            for (Participant p : team){
+                System.out.printf("%s,%s,%s,%s,%d,%s,%d,%s%n",
+                        p.getId(),
+                        p.getFullName(),
+                        p.getEmail(),
+                        p.getInterest(),
+                        p.getSkillLevel(),
+                        p.getRole(),
+                        p.getPersonalityScore(),
+                        p.getPersonalityType()
+                );
+            }
+            System.out.println(); //the blnak line for the team breaks
+        }
+    }
+}
