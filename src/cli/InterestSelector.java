58c0171b4ab20de6dbb50b6402c9c4a78266a4d1
mithulya-s <mkumarapeli4@gmail.com>
2025-11-15 12:15:59 +0530

    Fixed scanner instances

--- src/cli/InterestSelector.java
+++ src/cli/InterestSelector.java
@@ -5,44 +5,67 @@ import utilities.Interest;
 import java.util.Scanner;
 
 public class InterestSelector {
-    public InterestSelector(Scanner surveyScanner) {
+    private final Scanner scanner;
+
+    public InterestSelector(Scanner scanner) {
+        this.scanner = scanner;
         //takes in the stuff from the survey class
     }
 
     // cli functions for all the parts of the interest section of the survey
 
     public Interest promptForInterest() {
-        Scanner sc = new Scanner(System.in);
-        Interest selectedInterest = null;
+        displayMenu();
+        while (true) {
+            String userInp = scanner.nextLine().trim();
 
-        while (selectedInterest == null) {
-            System.out.println("-------------------------------------------------------");
-            System.out.println("Please enter the number of your preferred interest:    ");
-            System.out.println("-------------------------------------------------------");
-            System.out.println("1 - FIFA");
-            System.out.println("2 - CS:GO");
-            System.out.println("3 - Valorant");
-            System.out.println("4 - Dota");
-            System.out.println("5 - Chess");
-            System.out.println("6 - Basketball");
-            System.out.println("-------------------------------------------------------");
+            //vldations
+            if (userInp.isEmpty()) {
+                System.out.println("Please enter a value between 1 and 6\n");
+                continue;
+            }
 
-            int userInp = sc.nextInt();
-            selectedInterest = switch (userInp) {
-                case 1 -> Interest.FIFA;
-                case 2 -> Interest.CSGO;
-                case 3 -> Interest.VALORANT;
-                case 4 -> Interest.DOTA;
-                case 5 -> Interest.CHESS;
-                case 6 -> Interest.BASKETBALL;
-                default -> null;
-            };
+            //change the type
+            try {
+                int value = Integer.parseInt(userInp);
+                Interest selectedInterest = mapValueToInterest(value);
 
-            if (selectedInterest == null) {
-                System.out.println("Invalid input. Please try again.");
+                if (selectedInterest == null) {
+                    System.out.println("Invalid choice. Please enter a value between 1 and 6\n");
+                }else  {
+                    return selectedInterest;
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid choice. Please enter a value between 1 and 6\n");
             }
         }
+    }
 
-        return selectedInterest;
+    private void displayMenu() {
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Please enter the number of your preferred interest:    ");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("1 - FIFA");
+        System.out.println("2 - CS:GO");
+        System.out.println("3 - Valorant");
+        System.out.println("4 - Dota");
+        System.out.println("5 - Chess");
+        System.out.println("6 - Basketball");
+        System.out.println("-------------------------------------------------------");
+
+
+    }
+
+    private Interest mapValueToInterest(int value) {
+        return switch (value) {
+            case 1 -> Interest.FIFA;
+            case 2 -> Interest.CSGO;
+            case 3 -> Interest.VALORANT;
+            case 4 -> Interest.DOTA;
+            case 5 -> Interest.CHESS;
+            case 6 -> Interest.BASKETBALL;
+            default -> null;
+        };
     }
 }
