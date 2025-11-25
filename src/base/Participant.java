0efb59cc6c471d9c144686fc41525761c902bce1
mithulya-s <mkumarapeli4@gmail.com>
2025-11-25 16:11:12 +0530

    Formatting enhancements

--- src/base/Participant.java
+++ src/base/Participant.java
@@ -7,9 +7,6 @@ import utilities.Role;
 //Represents a participant in the forming system, this is the template for creating a participant.
 
 public class Participant {
-    //Represents a participant in the forming system, this is the template for creating a participant.
-
-
     private  String id;
     private  String fullName;
     private  String email;
