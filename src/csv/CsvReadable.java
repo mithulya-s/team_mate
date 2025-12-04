398555bde70f4d42e440641ffe4df386babf8603
mithulya-s <mkumarapeli4@gmail.com>
2025-12-04 15:21:43 +0530

    Polished OOP documentation

--- src/csv/CsvReadable.java
+++ src/csv/CsvReadable.java
@@ -1,5 +1,12 @@
 package csv;
 
+/*
+ - This class:
+        - Generic contract for reading objects from a CSV file.
+        - Provides abstraction for CSV readers, ensuring consistency across different implementations.
+        - Helps with separation of concerns so that the parsing logic is encapsulated in implementing classes,
+            while higher layers only depend on this interface.
+ */
 public interface CsvReadable<T> {
     T readFromCsv(String filePath);
 }
