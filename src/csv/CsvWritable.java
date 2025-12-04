05f4ba7c75f26fa9908d10268d2cda480891f83d
mithulya-s <mkumarapeli4@gmail.com>
2025-12-04 15:23:55 +0530

    Polished OOP documentation

--- src/csv/CsvWritable.java
+++ src/csv/CsvWritable.java
@@ -3,6 +3,13 @@ package csv;
 import java.io.IOException;
 import java.util.List;
 
+/*
+This class:
+      -  Generic contract for writing objects to a CSV file.
+      - Provides abstraction for CSV writers, ensuring consistency across different implementations.
+      - Helps with separation of concerns so that the logic is encapsulated in implementing classes,
+         while higher layers only depend on this interface.
+*/
 public interface CsvWritable<T> {
     void writeToCsv(List<T> items, String filePath) throws IOException;
 }
